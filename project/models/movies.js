20b2be441edbd25ead0ece8ee6efc610506857a6
Rachel Jackson <racheljackson@Rachels-MacBook-Pro-2.local>
2016-06-17 16:28:02 -0400

    updatd movies model

--- project/models/movies.js
+++ project/models/movies.js
@@ -6,8 +6,19 @@ var mongoose = require('mongoose');
 // ===============================
 // MODEL SCHEMA
 // ===============================
+var MoviesSchema = mongoose.Schema({
+	id: String, 
+	title: String, 
+	img: String, 
+	description: String, 
+	watched: Boolean, 
+	rating: String
+});
 
+var Movies = mongoose.model('Movies', MoviesSchema);
 
 // ===============================
 // ROUTE IT OUT
 // ===============================
+
+module.exports = Movies;
