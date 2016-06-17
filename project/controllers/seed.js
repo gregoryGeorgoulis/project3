14d45f59818d0efebdc1fbb4a2442e2b161b376d
Rachel Jackson <racheljackson@Rachels-MacBook-Pro-2.local>
2016-06-17 17:01:18 -0400

    created seed.js unfortunately on the user-models branch

--- project/controllers/seed.js
+++ project/controllers/seed.js
@@ -19,21 +19,21 @@ router.get('/', function(req, res) {
 	});
 
 	var movie1 = new Movies({
-		id: ,
-		title: ,
-		img: ,
-		description: , 
-		watched: , 
-		rating: 
+		imdbID: "tt2294629",
+		title: "Frozen",
+		poster: "http://ia.media-imdb.com/images/M/MV5BMTQ1MjQwMTE5OF5BMl5BanBnXkFtZTgwNjk3MTcyMDE@._V1_SX300.jpg",
+		description: "When the newly crowned Queen Elsa accidentally uses her power to turn things into ice to curse her home in infinite winter, her sister, Anna, teams up with a mountain man, his playful reindeer, and a snowman to change the weather condition.", 
+		watched: false, 
+		rating: "5"
 	});
 
 	var movie2 = new Movies({
-		id: ,
-		title: ,
-		img: ,
-		description: , 
-		watched: , 
-		rating: 
+		imdbID: "tt0387564",
+		title: "Saw",
+		poster: "http://ia.media-imdb.com/images/M/MV5BMjAyNTcxNzYwMV5BMl5BanBnXkFtZTgwMzQzNzM5MjE@._V1_SX300.jpg",
+		description: "Two strangers awaken in a room with no recollection of how they got there or why, and soon discover they are pawns in a deadly game perpetrated by a notorious serial killer.", 
+		watched: false, 
+		rating: "4"
 	});
 
 	user1.save();
