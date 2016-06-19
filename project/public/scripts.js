31796d9ff3aab13a386051f7bcd18eeaebdcd97f
Cat Georgoulis <catmou@gregorys-MacBook-Pro.local>
2016-06-19 10:12:30 -0400

    scripts login and show

---
+++ project/public/scripts.js
@@ -0,0 +1,159 @@
+// ==========================
+// KING COMPONENT
+// ==========================
+
+var KingComponent = React.createClass({
+	getInitialState: function(){//initial state of user logged in
+		var userCheck;
+		if(document.cookie){
+			userCheck = false;
+		} else {
+			userCheck = "";
+		}
+		return{
+			username: "",
+			authUser: userCheck,
+			id: "",
+			movies:[],
+		};
+	},
+	changeLogin: function(){//state is set to the cookies of username and id
+		this.setState({
+			authUser: true, 
+			username: Cookies("username"),
+			id: Cookies("id")
+		});
+		this.showAjax();
+	},
+	getFwakingData: function(movies) {//This method gets our data and sets the state of 
+		this.setState({movies:movies});//The state of getFwakingData is now set to movies: title(which is the data we got back)
+	},
+	showAjax: function() {//This ajax call queries the database on the frontend via the get request in the users.js controller
+		$.ajax({//get users by ID
+			url:"/users/" + this.state.id,
+			method:"GET",
+			success: function(data) {//upon success we set the var title to data we received
+				console.log("==========================");
+				console.log(data.movies);
+				console.log('=======================');
+				var daMovies = data.movies;
+				var title = data.movies[0].title;
+				var description = data.movies[0].description;
+				var poster = data.movies[0].poster;
+				console.log('V', title);
+				// var both = data.movies.forEach(function(movie) {
+				// 	console.log('=======================');
+				// 	console.log(movie.title);
+				// 	console.log(movie.description);
+				// 	console.log(movie.rating);							
+				 // this.setState({movies: title});
+				 this.getFwakingData(daMovies);//we then invoke getFwakingData(which sets the state of movies) with our new data.
+			}.bind(this),
+			error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+		})
+	},
+	render: function(){
+		console.log("====> authUser state: ", this.state.authUser);
+		console.log("====> state of username: ", this.state.username);
+		console.log("====> state of id: ", this.state.id);
+		console.log("====> state of movies: ", this.state.movies);
+		if(this.state.authUser === true){
+			return(
+				<div>
+					<ShowUser movies={this.state.movies} name={this.state.username} />
+				</div>
+			);
+		} else {
+			return(
+				<div>
+					<h1>My fwak is dirty!</h1>
+					<FwakingLogin loginCheck={this.state.authUser} onChange={this.changeLogin} />
+				</div>
+			);
+		}
+	}
+});
+
+var FwakingLogin = React.createClass({
+	getInitialState: function(){//Initial state is set to the props of auth user via the attribute "loginCheck"
+		return{
+			username: this.props.loginCheck,
+			password: this.props.loginCheck,
+			loginStatus: this.props.loginCheck
+		};	
+	},
+	handleLoginFormChange: function(stateName, e){
+		var change = {};
+		console.log("===> This is stateName: ", stateName);
+		change[stateName] = e.target.value;
+		this.setState(change);//sets state to the input of login form
+	},
+	handleSubmit: function(e){//The submit will be handled by passing the username and password through the loginAJAX call
+		e.preventDefault();
+		var username = this.state.username.trim();
+		var password = this.state.password.trim();
+		this.loginAJAX(username, password);
+	},
+	loginAJAX: function(username, password){
+		$.ajax({
+			url: "/auth",
+			method: "POST", 
+			data: {
+				username: username, 
+				password: password
+			},
+
+			success: function(data){
+				console.log("===>This is loginAJAX success data: ", data);
+				Cookies.set("jwt_token", data.token);
+				Cookies.set("username", data.username);
+				Cookies.set("id", data.id);
+				this.props.onChange(data.token)
+			}.bind(this)
+		});
+	},
+
+
+	render: function(){
+		return(
+			<div className="login-form" >
+        <h3>Please Login</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input className="username-login-form" type="text" placeholder="username" onChange={this.handleLoginFormChange.bind(this, 'username')}/>
+          <br/>
+          <label htmlFor="password">Password</label>
+          <input className="password-login-form" type="text" placeholder="password" onChange={this.handleLoginFormChange.bind(this, 'password')}/>
+          <br/>
+          <input type="submit"/>
+        </form>
+    </div>
+		);
+	}
+})
+
+
+var ShowUser =React.createClass({
+	render: function() {
+		console.log("props ==>", this.props);
+		console.log(this.props.movies);
+		// var movies = this.props.movies.map(function(movie){})
+		
+		return(
+			<div>
+				{movies}
+			</div>
+
+		);
+	}
+})
+
+// ==========================
+//  REACT DOM
+// ==========================
+
+ReactDOM.render(
+	<KingComponent />, 
+document.getElementById("container"));
